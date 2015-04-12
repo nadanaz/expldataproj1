d85b0ce95dc3b41d86bdf16cb765eb55c947a598
nadan <nguyenandan@gmail.com>
2015-04-13 00:31:22 +0700

    Modify minor bug in plot4.R. Auto load data in each plot script

--- src/plot3.R
+++ src/plot3.R
@@ -1,6 +1,7 @@
 # setting up the data
 # actually we do not need this if we run the scripts next to each other
 # but we assume that we do not
+source("read_data.R")
 datetime = paste(data$Date, data$Time)
 datetime <- as.POSIXct(strptime(datetime, "%d/%m/%Y %H:%M:%S"))
 Sys.setlocale("LC_TIME", "en_US.UTF-8")
--- src/plot4.R
+++ src/plot4.R
@@ -1,6 +1,7 @@
 # setting up the data
 # actually we do not need this if we run the scripts next to each other
 # but we assume that we do not
+source("read_data.R")
 datetime = paste(data$Date, data$Time)
 datetime <- as.POSIXct(strptime(datetime, "%d/%m/%Y %H:%M:%S"))
 Sys.setlocale("LC_TIME", "en_US.UTF-8")
@@ -10,7 +11,7 @@ png(filename = "../figure/plot4.png", width = 480, height = 480, units = "px")
 par(mfrow=c(2, 2), mar=c(4, 4, 1, 1), oma=c(1.5, 1.5, 0.5, 0.5))
 plot(datetime, data$Global_active_power, type="l", 
      ylab="Global Active Power", xlab="")
-plot(datetime, data$Voltage, type="l", ylab="Vontage")
+plot(datetime, data$Voltage, type="l", ylab="Voltage")
 plot(datetime, data$Sub_metering_1, col="black", type="l", 
      xlab="", ylab="Energy sub metering")
 lines(datetime, data$Sub_metering_2, col="red", type="l")
